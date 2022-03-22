86751649b839ebfbc88f927f69b985bea8d72eaf
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-22 13:05:37 +0530

    Add project files.

---
+++ TSystems_Multithreading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TSystems_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread1 demo = new Thread1();
+            Thread t1 = new Thread(new ThreadStart(demo.Test));
+            Thread t2 = new Thread(new ThreadStart(demo.Test2));
+            Thread t3 = new Thread(new ThreadStart(demo.Test3));
+            t1.Name = "First Thread";
+            t2.Name = "Second Thread";
+            t3.Name = "Third Thread";
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.Normal;
+            t3.Priority = ThreadPriority.Highest;
+            
+
+            t1.Start();
+            t2.Start();
+            
+            t3.Start();
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TSystems_Multithreading/Thread.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TSystems_Multithreading
+{
+   public class Thread1
+    {
+        public void Test()
+        {
+            lock (this)
+            { 
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+
+
+
+            }
+        }
+        public void Test2()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+        public void Test3()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+    }
+}
